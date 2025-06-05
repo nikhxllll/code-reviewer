e9de50fad178c48b692396f8ca4ac3b1e98bc054
Nikhil Yadav <ny619619@gmail.com>
2025-06-05 20:07:52 +0530

    changes in app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const aiRoutes  = require('./router/ai.routes')
 const app= express()
 
+app.use(express.json())
+
 app.get('/',(req,res)=>{
     res.send("Hello world")
 })
